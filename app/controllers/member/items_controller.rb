ea999f7d3e7e4cbeda4cbd34146a9542751ea9b9
shushikata <shu.42111227@gmail.com>
2020-08-17 13:39:38 +0000

    update_items[show,index]

--- app/controllers/member/items_controller.rb
+++ app/controllers/member/items_controller.rb
@@ -1,7 +1,10 @@
 class Member::ItemsController < ApplicationController
   def index
+    @items = Item.all
+    @items = Item.page(params[:page]).per(8)
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  
+  # belongs_to :genre
+  # has_many :order_details, dependent: :destroy
+  has_many :cart_items, dependent: :destroy
+
+  attachment :image
+
 end
