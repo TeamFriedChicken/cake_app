0eb040132a4f3ae901268de9cde8f1894a99bec2
taku-WC <pio.neer.bi10@gmail.com>
2020-08-18 18:22:31 +0900

    fix devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+    	:email,
+    	:last_name,
+		:first_name,
+		:kana_last_name,
+		:kana_first_name,
+		:phone_number,
+		:address,
+		:postcode,])
+  end
 end
