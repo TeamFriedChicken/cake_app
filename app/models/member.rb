c459cdcd21fbd0fbe587ace8351de5c7a32682ad
shushikata <shu.42111227@gmail.com>
2020-08-21 07:06:20 +0000

    pull develop

--- app/models/member.rb
+++ app/models/member.rb
@@ -5,5 +5,5 @@ class Member < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :cart_items, dependent: :destroy
-  has_many :cart_items, dependent: :destroy
+  has_many :orders, dependent: :destroy
 end
