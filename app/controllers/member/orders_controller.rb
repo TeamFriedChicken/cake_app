25e9adfff701a6d02d98689b86f2632e4f80564f
vagrant <sotahashimoto316@gmail.com>
2020-08-21 07:06:18 +0000

    test

--- app/controllers/member/orders_controller.rb
+++ app/controllers/member/orders_controller.rb
@@ -1,11 +1,30 @@
 class Member::OrdersController < ApplicationController
+  before_action :authenticate_member!
+  before_action :set_member
+
   def new
+    @order = Order.new
   end
 
   def confirm
+    @order = Order.new
+    @cart_items = current_member.cart_items
   end
 
   def create
+    current_member.cart_items.exists?
+    @order = Order.new(order_params)
+    @order.member_id = current_member.id
+    @order.save
+    current_member.cart_items.each do |cart_item|
+      order_item = @order.order_items.build
+      order_item.order_id = @order.id
+      order_item.product_id = cart_item.product_id
+      order_item.quantity = cart_item.quantity
+      order_item.order_price = cart_item.product.price
+      order_item.save
+      cart_item.destroy #order_itemに情報を移したらcart_itemは消去
+    end
   end
 
   def complete
@@ -15,5 +34,18 @@ class Member::OrdersController < ApplicationController
   end
 
   def show
+  end
+
+  private
+
+  def set_member
+    @member = current_member
+  end
+
+  def order_params
+    params.require(:order).permit(
+      :created_at, :member_id, :postage, :status,
+      order_details_attributes: [:item_id, :order_id, :quantity, :purchase]
+      )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -29,9 +29,9 @@ Rails.application.routes.draw do
 
     end
 
-    resources :orders, only: [:new, :create, :show, :index]
-    get 'orders/comfirm', to: 'orders#comfirm'
+    get 'orders/confirm', to: 'orders#confirm'
     get 'orders/complete', to: 'orders#complete'
+    resources :orders, only: [:new, :create, :show, :index]
 
     get 'members/:id/resignation', to: 'members#resignation'
     patch 'members/:id', to: 'members#quit'
