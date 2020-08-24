f57f66345074b5ec1924981c41fa98c22f4dcc3c
vagrant <sotahashimoto316@gmail.com>
2020-08-24 06:46:52 +0000

    admin_order

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,14 +1,10 @@
 class CartItem < ApplicationRecord
-
   belongs_to :item
   belongs_to :member
 
-
   validates :member_id, :item_id, :quantity, presence: true
-
 
   def total_price
 		item.price*quantity
   end
-  
 end
