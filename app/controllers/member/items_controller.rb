5ef24088bfedd7b422a4bfaa3f70d0f14e0a987e
shushikata <shu.42111227@gmail.com>
2020-08-27 15:50:40 +0000

    fix all

--- app/controllers/member/items_controller.rb
+++ app/controllers/member/items_controller.rb
@@ -1,10 +1,9 @@
 class Member::ItemsController < ApplicationController
-
   def index
     @genres = Genre.where(is_active: true)
 
     if params[:genre].nil?
-      @items = Item.where(is_active: true).page(params[:page]).per(8)
+      @items = Item.page(params[:page]).per(8)
     else
       @items = Item.where(genre_id: params[:genre]).page(params[:page]).per(8)
     end
@@ -12,7 +11,7 @@ class Member::ItemsController < ApplicationController
   end
 
   def show
-    @genres = Genre.where(is_active: "true")
+    @genres = Genre.all
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
   end
