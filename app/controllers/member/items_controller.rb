aec920af735292aa4900fa96fe98a8a2710ce831
vagrant <sotahashimoto316@gmail.com>
2020-08-28 06:14:08 +0000

    ransack

--- app/controllers/member/items_controller.rb
+++ app/controllers/member/items_controller.rb
@@ -1,17 +1,10 @@
 class Member::ItemsController < ApplicationController
-  # def index
-  #   @genres = Genre.where(is_active: true)
-  #   if params[:genre].nil?
-  #     @items = Item.where(is_active: true).page(params[:page]).per(8)
-  #   else
-  #     @items = Item.where(genre_id: params[:genre]).page(params[:page]).per(8)
-  #   end
-  # end
 
   def index
     @genres = Genre.where(is_active: true)
+    # URLに:genre_idが存在するならif、しないならelse
     if params[:genre_id].present?
-      @items = Item.where(genre_id: params[:genre_id],is_active: true)
+      @items = Item.where(genre_id: params[:genre_id])
     else
       # genres有効の商品のみ表示
       @items = Item.joins(:genre).where(genres: {is_active: "true"})
