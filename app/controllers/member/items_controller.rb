809c51a0ba3136e0e22a4519787e073ced0ce217
taku-WC <pio.neer.bi10@gmail.com>
2020-08-29 16:22:01 +0900

    c

--- app/controllers/member/items_controller.rb
+++ app/controllers/member/items_controller.rb
@@ -5,9 +5,11 @@ class Member::ItemsController < ApplicationController
     if params[:genre_id].present?
       @items = Item.where(genre_id: params[:genre_id]).page(params[:page]).per(8)
       @genre = Genre.find(params[:genre_id])
+      @item_all = Item.where(genre_id: params[:genre_id])
     else
       # genres有効の商品のみ表示
       @items = Item.joins(:genre).where(genres: {is_active: "true"}).page(params[:page]).per(8)
+      @item_all = Item.joins(:genre).where(genres: {is_active: "true"})
     end
   end
 
